35692da5ed719a4818c260345763fa3d1d6d1a3a
sermarmar <sermarmar1995@gmail.com>
2025-12-01 18:20:59 +0100

    feat: agregar gestión de clientes con rutas, controladores y servicios

---
+++ backend/src/ports/clientPort.js
@@ -0,0 +1,19 @@
+export class ClientPort {
+
+    async findAll() {
+        throw new Error("findAll Not implemented");
+    }
+
+    async findById(id) {
+        throw new Error("findById Not implemented");
+    }
+
+    async create(client) {
+        throw new Error("create Not implemented");
+    }
+
+    async update(id, client) {
+        throw new Error("update Not implemented");
+    }
+    
+}
